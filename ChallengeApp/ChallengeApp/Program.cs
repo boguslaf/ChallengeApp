65cdd7e7825345344b2fca094793156e2a635209
boguslaf <boguslaf@wp.pl>
2023-11-19 12:09:55 +0100

    Zadanie dnia czwartego

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! Tu Bodek, Jestem gotów!");
+﻿// Zadanie dnia czwartego
+
+string name = "Ewa";
+bool isWoman = true;
+int age = 33;
+
+if (isWoman == true && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (isWoman == true && age == 33 && name == "Ewa")
+{
+    Console.WriteLine("Ewa, lat 33");
+}
+else if (isWoman == false && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    Console.WriteLine("Nieznana osoba.");
+}
+    
+var keyInfo = Console.ReadKey(true); // w wersji 17.8.0 Visual Studio program zamyka się automatycznie
+
